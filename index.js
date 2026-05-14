509b02c4ae780630de6c286c21a20e38cd863845
Katherine <katheife2002@gmail.com>
2026-05-14 13:41:13 +0100

    Updated index.js for redeployment

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const port = 7077;
 app.use(cors());
 app.use(express.json());
 
+//open the channel of communication with mongodb
+//const local_url = "mongodb://localhost:27017/UserDB"; // for mongo compass
+const live_url = 
+"mongodb+srv://dev_Katherine:db_Katherine@cluster0.sfe7x8c.mongodb.net/ToDoDB?appName=Cluster0"
+mongoose.connect(live_url) 
+.then(() => console.log("MongoDB connected")) 
+.catch((err) => console.error("Connection Error: ", err)) 
+
+//import router 
+const router = require("./Routes/todoroutes");
+
+//create an instance of your router.
+// use routes
+app.use("/todos", router);
+
 //define the route for testing
 app.get("/", (req, res) => {
     res.send("Welcome onboard!");
@@ -21,15 +36,3 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-//open the channel of communication with mongodb
-const local_url = "mongodb://localhost:27017/UserDB"; // for mongo compass
-mongoose.connect(local_url) 
-.then(() => console.log("MongoDB connected")) 
-.catch((err) => console.error("Connection Error: ", err)) 
-
-//import router 
-const router = require("./Routes/todoroutes");
-
-//create an instance of your router.
-// use routes
-app.use("/todos", router);
